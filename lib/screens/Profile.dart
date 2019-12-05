73fb5037cdfb2dfa2af93baf4d754c5e5a4be472
Jad <jad_slim@live.com>
2019-12-04 21:35:05 -0500

    finsihed login

--- lib/screens/Profile.dart
+++ lib/screens/Profile.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import '../screens/loginPage.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import '../models/user.dart';
 
 class ProfileNotLoggedIn extends StatelessWidget {
 
@@ -127,35 +125,6 @@ class ProfileNotLoggedIn extends StatelessWidget {
             ],
             ),
         ),
-          // FlatButton(child: Text("sign out"), onPressed: () async {
-
-        //     try {
-        //       FirebaseAuth.instance.signOut();
-        //     } catch(e) {
-        //          print(e);
-        // showDialog(
-        //     context: context,
-        //     builder: (BuildContext context) {
-        //       return AlertDialog(
-        //         title: new Text("Error"),
-        //         content: new Text(e.toString()),
-        //         actions: <Widget>[
-        //           // usually buttons at the bottom of the dialog
-        //           new FlatButton(
-        //             child: new Text("Close"),
-        //             onPressed: () {
-        //               Navigator.of(context).pop();
-        //             },
-        //           ),
-        //         ],
-        //       );
-        //     });
-
-        //     } finally {
-        //       User(uid: null);
-        //     }
-          // },
-          // )
          ],
        ),
        
