b7a26aed5257ca35b9e000398ab27312547476e0
Jad <jad_slim@live.com>
2019-12-25 10:21:40 -0500

    added some error boxes

--- lib/widgets/Loader.dart
+++ lib/widgets/Loader.dart
@@ -18,17 +18,26 @@ class Loader extends StatelessWidget {
       body: Center(
         child: Container(
           decoration: BoxDecoration(
-                          color: Colors.white,
-
-            borderRadius: new BorderRadius.only(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  blurRadius: 1, // has the effect of softening the shadow
+                  // spreadRadius: 0, // has the effect of extending the shadow
+                  // offset: Offset(
+                  //   10.0, // horizontal, move right 10
+                  //   10.0, // vertical, move down 10
+                  // ),
+                )
+              ],
+              borderRadius: new BorderRadius.only(
                   topLeft: const Radius.circular(20.0),
                   topRight: const Radius.circular(20.0),
-                                       bottomLeft: const Radius.circular(20.0),
-                  bottomRight: const Radius.circular(20.0))
-          ),
+                  bottomLeft: const Radius.circular(20.0),
+                  bottomRight: const Radius.circular(20.0))),
           constraints: BoxConstraints(minWidth: 250, maxWidth: 250),
-          height: MediaQuery.of(context).size.width/1.5,
-          width: MediaQuery.of(context).size.width/1.5,
+          height: MediaQuery.of(context).size.width / 1.5,
+          width: MediaQuery.of(context).size.width / 1.5,
           child: Padding(
             padding: const EdgeInsets.all(20.0),
             child: Column(
@@ -36,11 +45,14 @@ class Loader extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
                 CircularProgressIndicator(
-                  strokeWidth: 2,
+                    strokeWidth: 2,
                     valueColor: AlwaysStoppedAnimation<Color>(kMainColor)),
                 Padding(
                   padding: const EdgeInsets.only(top: 30.0),
-                  child: Text(loaderText, style: TextStyle(fontWeight: FontWeight.w700, fontSize: 15),),
+                  child: Text(
+                    loaderText,
+                    style: TextStyle(fontWeight: FontWeight.w700, fontSize: 15),
+                  ),
                 )
               ],
             ),
