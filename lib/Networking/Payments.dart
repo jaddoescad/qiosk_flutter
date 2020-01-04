9342e114ff243a3db2425f03bc3033ce3840edaa
Random <random@Randoms-MacBook-Air.local>
2020-01-04 14:31:55 -0500

    brought back payment

--- lib/Networking/Payments.dart
+++ lib/Networking/Payments.dart
@@ -6,6 +6,7 @@ import 'package:flutter_stripe_payment/flutter_stripe_payment.dart';
 import 'package:cloud_functions/cloud_functions.dart';
 import '../Networking/Orders.dart';
 import '../models/payment.dart';
+import 'package:stripe_payment/stripe_payment.dart';
 
 enum PageToGo { Checkout, AddCard, Auth }
 enum AddPaymentStatus { Success, Failed }
@@ -61,41 +62,41 @@ class Payments {
   }
 
   static Future pay(uid, stripeId, orderId, subtotal, taxes, total, currency, cart, rid, context, rname) async {
-    // final token = Provider.of<PaymentModel>(context).token;
+    final token = Provider.of<PaymentModel>(context).token;
 
-    // CloudFunctions cf = CloudFunctions();
-    // HttpsCallable callable = cf.getHttpsCallable(
-    //   functionName: 'createPaymentIntent',
-    // );
-    // var resp = await callable.call(<String, dynamic>{
-    //   'uid': uid.toString(),
-    //   'stripeId': stripeId,
-    //   'orderId': orderId,
-    //   'token': token,
-    //   'amount': total,
-    // });
+    CloudFunctions cf = CloudFunctions();
+    HttpsCallable callable = cf.getHttpsCallable(
+      functionName: 'createPaymentIntent',
+    );
+    var resp = await callable.call(<String, dynamic>{
+      'uid': uid.toString(),
+      'stripeId': stripeId,
+      'orderId': orderId,
+      'token': token,
+      'amount': total,
+    });
 
-    // if (resp.data.containsKey('error')) {
-      // throw ('there was an error processing the payment ${resp.data['error'].toString()}');
-    // } else {
-      // await StripePayment.confirmPaymentIntent(
-      //   PaymentIntent(
-      //     clientSecret: resp.data['response']['client_secret'],
-      //     paymentMethodId: resp.data['sourceId'],
-      //   ),
-      // ).then((paymentIntent) async {
-      //   print(paymentIntent.status);
-      //   if (paymentIntent.status == 'succeeded') {
-      //     print('payment succeeded');
-          await OrdersNetworking().createOrder(orderId, cart, subtotal, taxes, total, uid, rid, context, DateTime.now().millisecondsSinceEpoch, rname);
-      //   } else {
-      //     print(paymentIntent.status);
-      //     throw ('error processing payment');
-      //   }
-      // }).catchError((setError) {
-      //   print(setError.toString());
-      //   throw (setError.toString());
-      // });
-    // }
+    if (resp.data.containsKey('error')) {
+      throw ('there was an error processing the payment ${resp.data['error'].toString()}');
+    } else {
+      await StripePayment.confirmPaymentIntent(
+        PaymentIntent(
+          clientSecret: resp.data['response']['client_secret'],
+          paymentMethodId: resp.data['sourceId'],
+        ),
+      ).then((paymentIntent) async {
+        print(paymentIntent.status);
+        if (paymentIntent.status == 'succeeded') {
+          print('payment succeeded');
+          await OrdersNetworking().createOrder(orderId, cart, subtotal, taxes, total, uid, rid, context, token, rname);
+        } else {
+          print(paymentIntent.status);
+          throw ('error processing payment');
+        }
+      }).catchError((setError) {
+        print(setError.toString());
+        throw (setError.toString());
+      });
+    }
   }
 }
